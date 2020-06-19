b5beaeaed0b903a91615eb21c8d4578ada4819e2
RichWarrior <faruk_thecno@hotmail.com>
2020-06-19 20:25:59 +0300

    Test

--- test/net.standart.redis.locker.test/LockerTest.cs
+++ test/net.standart.redis.locker.test/LockerTest.cs
@@ -1,6 +1,26 @@
-﻿namespace net.standart.redis.locker.test
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace net.standart.redis.locker.test
 {
     public class LockerTest
     {
+        [Fact]
+        public void Lock_Test()
+        {
+            string connString = "";
+            using (ILocker locker = new Locker(new List<ConnectionMultiplexer> { ConnectionMultiplexer.Connect(connString)}))
+            {
+                Lock lockObject;
+                enumLockStatus lockStatus =  locker.Lock("key", TimeSpan.FromSeconds(30), out lockObject);
+                if (lockStatus == enumLockStatus.Succeeded)
+                    Assert.True(true);
+                else
+                    Assert.True(false);
+            }
+        }
     }
 }
